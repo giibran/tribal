c2f28c54477ef593c6bf30a5631b429d77e211e3
Gibran Arias <gibran.arias@gmail.com>
2022-03-09 02:07:07 -0600

    Add page definition and single test

---
+++ cypress/integration/single_test.spec.js
@@ -0,0 +1,17 @@
+import job_openings_page from '../page_definition/job_openings.js';
+describe('Single test', () => {
+    beforeEach(() => {
+      cy.visit('https://boards.greenhouse.io/tribalcredit')
+    })
+
+    it('search Senior QA Engineer when filter by engineering and mexico', () => {
+        cy.url().should('eq', 'https://boards.greenhouse.io/tribalcredit')
+        cy.get(job_openings_page.logo).should('exist')
+        cy.get(job_openings_page.department_select).click()
+        cy.get('[id="select2-drop"]').find('[class="select2-result-label"]').contains('Engineering').click()
+        cy.get(job_openings_page.office_select).click()
+        cy.get('[id="select2-drop"]').find('[class="select2-result-label"]').contains('Mexico').click()
+        cy.get(job_openings_page.job_container).filter(':visible').contains('Sr. QA Engineer').should('exist')
+    })
+
+  })
---
+++ cypress/page_definition/job_openings.js
@@ -0,0 +1,8 @@
+const job_openings_page = {
+    logo: '[id="logo"]',
+    department_select: '[id="s2id_departments-select"]',
+    office_select: '[id="s2id_offices-select"]',
+    job_container: '[class="opening"]'
+}
+
+export default job_openings_page;
